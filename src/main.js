32fa94c638858a1e32f3cd0fe9c822ca268c018d
xinyeya <huafeilong2000@163.com>
2019-07-08 15:46:14 +0800

    '导入vant组件库'

--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import {
+  Button,
+  Tabbar,
+  TabbarItem
+} from 'vant'
+
+Vue
+  .use(Button)
+  .use(Tabbar)
+  .use(TabbarItem)
+
 Vue.config.productionTip = false
 
 new Vue({
