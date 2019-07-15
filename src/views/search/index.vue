63a0d81bb7299fead1bd594d562eb756d62f1504
xinyeya <huafeilong2000@163.com>
2019-07-15 17:46:35 +0800

    搜索历史记录-存储和获取历史记录

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -9,7 +9,7 @@
       />
     </form>
     <!-- 联想建议列表 -->
-    <van-cell-group>
+    <van-cell-group v-if="suggestions.length && searchText.length">
       <van-cell
         icon="search"
         v-for="item in suggestions"
@@ -26,7 +26,7 @@
     <!-- /联想建议列表 -->
 
     <!-- 历史记录 -->
-    <!-- <van-cell-group>
+    <van-cell-group v-else>
       <van-cell title="历史记录">
         <van-icon
           slot="right-icon"
@@ -34,7 +34,13 @@
           style="line-height: inherit;"
         />
       </van-cell>
-    </van-cell-group> -->
+      <van-cell
+        v-for="item in searchHistories"
+        :key="item"
+        :title="item"
+      >
+      </van-cell>
+    </van-cell-group>
     <!-- /历史记录 -->
   </div>
 </template>
@@ -47,7 +53,8 @@ export default {
   data () {
     return {
       searchText: '',
-      suggestions: []
+      suggestions: [],
+      searchHistories: JSON.parse(window.localStorage.getItem('search-histories')) // 搜索记录
     }
   },
 
@@ -75,12 +82,24 @@ export default {
     },
 
     handleSearch (q) {
-      this.$router.push({
-        name: 'search-result',
-        params: {
-          q
-        }
-      })
+      if (!q.length) {
+        return
+      }
+
+      this.searchHistories.push(q)
+
+      // 保存搜索历史记录
+      window.localStorage.setItem(
+        'search-histories',
+        JSON.stringify([...new Set(this.searchHistories)])
+      )
+      // 跳转到搜索页面
+      // this.$router.push({
+      //   name: 'search-result',
+      //   params: {
+      //     q
+      //   }
+      // })
       // this.$router.push('/search/'+q)
       // this.$router.push(`/search/${q}`)
     }
