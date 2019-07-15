dcc493d868846b21ec3a0c06cc1ffcc3577d95b7
xinyeya <huafeilong2000@163.com>
2019-07-15 17:03:54 +0800

    搜索结果-列表展示

--- src/views/search-result/index.vue
+++ src/views/search-result/index.vue
@@ -19,9 +19,9 @@
       @load="onLoad"
     >
       <van-cell
-        v-for="item in list"
-        :key="item"
-        :title="item"
+        v-for="item in articles"
+        :key="item.art_id.toString()"
+        :title="item.title"
       />
     </van-list>
     <!-- /文章列表 -->
@@ -29,31 +29,66 @@
 </template>
 
 <script>
+import { getSearch } from '@/api/search'
+
 export default {
   name: 'SearchResult',
   data () {
     return {
       list: [],
       loading: false,
-      finished: false
+      finished: false,
+      page: 1,
+      perPage: 10,
+      articles: []
     }
   },
 
-  methods: {
-    onLoad () {
-      // 异步更新数据
-      setTimeout(()=>{
-        for (let i = 0; i < 10; i++) {
-          this.list.push(this.list.length + 1)
-        }
-        // 加载状态结束
-        this.loading = false
+  computed: {
+    q () {
+      return this.router.params.q
+    }
+  },
 
-        // 数据全部加载完成
-        if (this.list.length >= 40) {
-          this.finished = true
-        }
-      }, 500)
+  async created () {
+    const data = await getSearch({
+      q: this.$route.params.q,
+      page: 1
+    })
+    console.log(data)
+  },
+
+  methods: {
+    async onLoad () {
+      await this.$sleep(800)
+
+      const data = await this.getSearchResults()
+
+      // 如果请求结果数组为空，则设置 List 组件已加载结束
+      if (!data.result.length) {
+        this.loading = false
+        this.finished = true
+        return
+      }
+
+      // 如果有数据，则将本次加载到的数据 push 到列表数组中
+      this.articles.push(...data.result)
+
+      // 数据加载完毕，更新当前页码为下一页，用于下一次加载更多
+      this.page += 1
+
+      // 结束当前加载的 loading
+      // List 列表组件每次 onLoad 会自动将 loading 设置为 true
+      // 如果你不设置的话，它不会触发下一次的 onload
+      this.loading = false
+    },
+
+    getSearchResults () {
+      return getSearch({
+        page: this.page,
+        perPage: this.perPage,
+        q: this.q
+      })
     }
   }
 }
