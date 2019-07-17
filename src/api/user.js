d0d2892f0c094b4030847d14214150e96d808ae2
xinyeya <huafeilong2000@163.com>
2019-07-17 20:22:40 +0800

    详情模块-关注用户-接口封装

--- src/api/user.js
+++ src/api/user.js
@@ -29,3 +29,26 @@ export const addBlacklist = userId => {
 		}
 	})
 }
+
+/**
+ * 关注用户
+ */
+export const followUser = userId => {
+  return request({
+    method: 'POST',
+    url: '/app/v1_0/user/followings',
+    data: {
+      target: userId
+    }
+  })
+}
+
+/**
+ * 取消关注用户
+ */
+export const unFollowUser = userId => {
+  return request({
+    method: 'DELETE',
+    url: `/app/v1_0/user/followings/${userId}`
+  })
+}
--- src/views/article/components/auth-info.vue
+++ src/views/article/components/auth-info.vue
@@ -8,7 +8,10 @@
       </div>
     </div>
     <div>
-      <van-button type="danger">关注</van-button>
+      <van-button
+        :type="article.is_followed ? 'default' : 'danger'"
+        @click="handleFollow"
+      >{{ article.is_followed ? '已关注' : '关注' }}</van-button>
     </div>
   </div>
 </template>
@@ -30,7 +33,11 @@ export default {
 
   computed: {},
 
-  methods: {}
+  methods: {
+    handleFollow () {
+      console.log('follow')
+    }
+  }
 }
 </script>
 
@@ -45,6 +52,7 @@ export default {
     align-items: center;
   }
   .avatar {
+    margin-right: 10px;
     width: 100px;
     height: 100px;
     border-radius: 100%;
